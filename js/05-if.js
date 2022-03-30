8211ee2b655d31b1f75b3a46a7fb48679e9a5c4b
rytisjakseboga <dzinglamon@gmail.com>
2022-03-30 13:11:02 +0300

    if

---
+++ js/05-if.js
@@ -0,0 +1,21 @@
+/*
+IF
+
+Sablonai:
+if () {}
+if () {} else {}
+
+Palyginimo operatoriai:
+- visi: <, >, <=, >=, ==, !=(nelygu), ===(griezta lygybe, naudoti, kai skiriasi tipai)
+- naudotini:
+- NEnaudotini: ==, !=
+*/
+
+const a = 7;
+const b = 55;
+
+if (a < b) {
+    console.log('A yra maziau uz B')
+} else {
+    console.log('A nera maziau uz B');
+}
