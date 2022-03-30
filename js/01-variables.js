9725ffcba99e0bf7e473dfc7826892ad7a90a051
rytisjakseboga <dzinglamon@gmail.com>
2022-03-30 10:36:28 +0300

    Numbers

---
+++ js/01-variables.js
@@ -0,0 +1,49 @@
+/*
+
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiksme tures keistis
+var - pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+
+Duomenu tipas:
+- number
+- string (tekstas)
+- boolean (logine reiksme: true / false)
+- array (masyvas, sarasas, matrica*)
+
+*/
+
+const luckyNumber = 123;
+console.log(luckyNumber);
+
+let pinigine = 5;
+console.log(pinigine);
+
+const vardas = 'Maryte';
+const pasisveikinimas = 'Laba diena, '+ vardas +'.'
+console.log('Laba diena, '+ vardas +'.');
+console.log(pasisveikinimas);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+console.log('Ar vedes?', arVedes);
+console.log('Ar meluoja del vaiku?', arMeluojaDelVaiku);
+console.log('Ar turi vaiku?', arTuriVaiku);
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai:', pazymiai)
+
+const studentai = ['Petras', 'Onute', 'Jonas']; 
+console.log(studentai);
+
+const booleanList = [true, false, true, false, true];
+console.log(booleanList);
+/*
+
+2*(2+2) = 8
+
+*/
+
---
+++ js/02-numbers.js
@@ -0,0 +1,67 @@
+/*
+Numbers:
+- normal:
+    - sveikieji
+    - desimtainiai
+- nenormalus:
+    - NaN (not-a-number)
+    - Infinity; -Infinity
+*/
+
+
+const sveikasis = 5;
+const desimtainiai = 3.14;
+const neigiamasDesimtainis = -3.14;
+const neSkaicius = NaN;
+const begalybe = Infinity;
+
+const pirmas = 'Labas';
+const antras = 'rytas';
+
+const dalmuo = pirmas / antras;
+console.log(dalmuo);
+
+console.log('num; num')
+console.log(7 + 5);
+console.log(7 - 5);
+console.log(7 * 5);
+console.log(7 / 5);
+
+console.log('num; string')
+console.log(7 + 'b');
+console.log(7 - 'b');
+console.log(7 * 'b');
+console.log(7 / 'b');
+
+console.log('string; num')
+console.log('a' + 5);
+console.log('a' - 5);
+console.log('a' * 5);
+console.log('a' / 5);
+
+console.log('string; string')
+console.log('a' + 'b');
+console.log('a' - 'b');
+console.log('a' * 'b');
+console.log('a' / 'b');
+
+console.log('num; true')
+console.log(7 + true);
+console.log(7 - true);
+console.log(7 * true);
+console.log(7 / true);
+
+
+console.log('true; num')
+console.log(true + 5);
+console.log(true - 5);
+console.log(true * 5);
+console.log(true / 5);
+
+console.log('false; num')
+console.log(false + 5);
+console.log(false - 5);
+console.log(false * 5);
+console.log(false / 5);
+
+
